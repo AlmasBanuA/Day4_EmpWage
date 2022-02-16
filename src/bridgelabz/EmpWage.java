e18b1f777082f063efda1f02d90310e75d0be296
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-16 20:35:32 +0530

    Solving using switch case statement

--- src/bridgelabz/EmpWage.java
+++ src/bridgelabz/EmpWage.java
@@ -2,14 +2,25 @@ package bridgelabz;
 
 public class EmpWage {
 
-	public static void main(String[] args) {
-		int iSFullTime = 1;
-        double empcheck= Math.floor(Math.random() *10) % 2;
-        if (empcheck == iSFullTime)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is Absent");
-
+    public static final int isPartTime = 2;
+    public static final int isFullTime = 1;
+    public static final int empRatePerHour = 20;
+    public static void main(String[] args) {
+        int empHrs = 0;
+        int totalWage = 0;
+        double empcheck= Math.floor(Math.random() *10) % 3;
+        switch ((int)empcheck){
+		case isFullTime:
+			empHrs=8;
+			break;
+		case isPartTime:
+			empHrs=4;
+			break;
+		default:
+			empHrs=0;
+			
 	}
-
+        totalWage = empHrs * empRatePerHour;
+        System.out.println("Emp WAge: " + totalWage);
+    }
 }
