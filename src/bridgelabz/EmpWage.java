cbbe991fb461842fb332a06602d85a584e02308b
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-16 20:30:29 +0530

    Calculating Daily Employee Wage

--- src/bridgelabz/EmpWage.java
+++ src/bridgelabz/EmpWage.java
@@ -7,7 +7,7 @@ public class EmpWage {
       int EmpRatePerHour = 20;
         int empHrs = 0;
 	int totalWage = 0;
-        double empcheck= Math.floor(Math.random() *10) % 2;
+        double empcheck= Math.floor( Math.random() *10) % 2;
         if (empcheck == isFullTime)
             empHrs = 8;
         totalWage = empHrs * EmpRatePerHour;
